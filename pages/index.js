0b04ad9b1dee31f44e57af0e76e42a638f34da02
towkir-turtle <towkir.webdeveloper@gmail.com>
2022-10-23 19:36:44 +0600

    created product section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import ProductItem from '../components/ProductItem';
+import data from '../utils/data';
 
 const Home = () => {
-  return <Layout title="Home"></Layout>;
+  return (
+    <Layout title="Home">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
